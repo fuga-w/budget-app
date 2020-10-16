8dbda91fcb6e99b22d6da2af1e1a6e6589158582
fuga-w <fugarian@protonmail.com>
2020-10-16 02:10:07 +0000

    Appの修正

--- 001/budget-app/src/index.js
+++ 001/budget-app/src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const App = () => <h1>test</h1>
+const App = () => <h1>Hello React!</h1>
 
 ReactDOM.render(<App />,document.getElementById('root'));
